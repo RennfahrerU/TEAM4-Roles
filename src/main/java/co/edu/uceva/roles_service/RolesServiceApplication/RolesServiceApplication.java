66ab3e1fe74cfbc09122fb23894d5e728b70ea95
Yilver <yilver.rivera01@uceva.edu.co>
2023-04-10 18:54:36 -0500

    .

---
+++ src/main/java/co/edu/uceva/roles_service/RolesServiceApplication/RolesServiceApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.roles_service.RolesServiceApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RolesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uceva/roles_service/RolesServiceApplication/RolesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.roles_service.RolesServiceApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
